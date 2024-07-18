d341acca06c9327cc8d37e72dec7b4f69fbca548
geraldbryan <geraldbryan9914@gmail.com>
2024-07-18 17:38:00 +0700

    update null price handler

--- null_price.py
+++ null_price.py
@@ -37,6 +37,17 @@ today_date = datetime.today().date()
 
 id_data = supabase.table("idx_daily_data").select("*").eq("date",today_date.strftime("%Y-%m-%d")).is_("close", "null").execute()
 
+daily_data = supabase.table("idx_daily_data").select("symbol").eq("date",today_date.strftime("%Y-%m-%d")).execute()
+daily_data = pd.DataFrame(daily_data.data).shape[0]
+
+active_stock = supabase.table("idx_active_company_profile").select("symbol").execute()
+active_stock = pd.DataFrame(active_stock.data).shape[0]
+
+if daily_data == active_stock:
+    logging.info(f"{daily_data} tickers are scraped and already same with the number of active stock")
+else:
+    logging.error(f"{daily_data} tickers are scraped not same with number of active stock")
+
 if pd.DataFrame(id_data.data).shape[0] == 0:
     logging.info(f"No null values at every stock on {today_date}")
 else:
