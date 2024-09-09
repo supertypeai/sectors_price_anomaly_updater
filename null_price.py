003d5b31eb92f9ae111705f10e8b789b197efff6
geraldbryan <geraldbryan9914@gmail.com>
2024-09-09 22:31:28 +0700

    add pipeline error notifications

--- null_price.py
+++ null_price.py
@@ -8,6 +8,11 @@ from datetime import datetime
 import datetime as dt
 import yfinance as yf
 import numpy as np
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv
+load_dotenv()
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
@@ -30,6 +35,28 @@ def convert_numpy_int64(data):
         return [convert_numpy_int64(i) for i in data]
     else:
         return data
+    
+def send_email(daily_data, active_stock,today_date):
+    # Create email details
+    message = Mail(
+        from_email='gerald@supertype.ai',
+        to_emails='geraldbryan9914@gmail.com',
+        subject='Sending with SendGrid is Fun',
+        html_content=f'<strong>The number of stock scrapped on {today_date} not equal with the number of active stocks, only {daily_data} from {active_stock} are not null, please check your script again!</strong>'
+    )
+    
+    try:
+        # Initialize SendGrid API client with your API key
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+        
+        # Send the email
+        response = sg.send(message)
+        print(f"Email sent! Status code: {response.status_code}")
+        print(response.body)  # Optional: Print the response body for debugging
+        print(response.headers)  # Optional: Print response headers for debugging
+    
+    except Exception as e:
+        print(f"Error sending email: {e}")
 
 initiate_logging(LOG_FILENAME)
 
@@ -46,32 +73,5 @@ active_stock = pd.DataFrame(active_stock.data).shape[0]
 if daily_data == active_stock:
     logging.info(f"{daily_data} tickers are scraped and already same with the number of active stock")
 else:
-    logging.error(f"{daily_data} tickers are scraped not same with number of active stock")
-
-if pd.DataFrame(id_data.data).shape[0] == 0:
-    logging.info(f"No null values at every stock on {today_date}")
-else:
-    null_data = pd.DataFrame(id_data.data)
-
-    logging.error(f"There are some null values on {today_date} for stocks {null_data.symbol.unique()}")
-
-    df_rerun = pd.DataFrame()
-
-    for i in id_data.symbol.unique():
-        a = yf.Ticker(i).history(start=today_date, end=today_date+dt.timedelta(1)).reset_index()[["Date","Close",'Volume']]
-        a['symbol'] = i
-
-        df_rerun = pd.concat([df_rerun,a])
-
-    df_rerun.Date = pd.to_datetime(df_rerun.Date)
-    df_rerun["Date"] = df_rerun["Date"].dt.strftime("%Y-%m-%d")
-    
-    logging.info(f"update data for {i} on {today_date}")
-
-    # for i in df_rerun.symbol.unique():
-    #     supabase.table("idx_daily_data").update(
-    #                 {"close": convert_numpy_int64(df_rerun[df_rerun.symbol == i].iloc[0]["Close"]),
-    #                 "volume": convert_numpy_int64(df_rerun[df_rerun.symbol == i].iloc[0]["Volume"])}
-    #             ).eq("symbol", i).eq("date", df_rerun[df_rerun.symbol == i].iloc[0]["Date"]).execute()
-    
-    #     logging.info(f"update data for {i} on {today_date}")
+    logging.error(f"{daily_data} tickers are scraped, not same with number of active stock, please checked the pipeline and revised it")
+    send_email(daily_data,active_stock,today_date)
